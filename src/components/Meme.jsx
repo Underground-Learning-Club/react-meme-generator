94f8f0754447e282144c8426f6d2d523af1ced06
Dasajton <david.sajitz.94@gmail.com>
2023-12-13 17:45:02 +0100

    function frame for random image rendering

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,9 +1,15 @@
 import '../CSS/Meme.scss'
+import memesData from '../memesData'
 
 const Meme = () => {
+  const getNewMemeImage = () => {
+    let randomNumber = Math.floor(Math.random() * 100) + 1
+    console.log(memesData.data.memes[randomNumber].url)
+  }
+
   return (
     <>
-      <form className="meme--form">
+      <div className="meme--form">
         <div className="meme--input-fields-wrapper">
           <div className="meme--input-field-wrapper">
             <label htmlFor="topText">Top text</label>
@@ -25,8 +31,10 @@ const Meme = () => {
           </div>
         </div>
 
-        <button className="meme--btn">Get new meme image</button>
-      </form>
+        <button className="meme--btn" onClick={getNewMemeImage}>
+          Get new meme image
+        </button>
+      </div>
     </>
   )
 }
